2ede25aa708456aad48cdb7dae095c5789e1af60
Stefan Bischoff <stefan.bischoff@9plus.de>
2020-05-06 01:19:36 +0200

    Added appinstance

--- main.go
+++ main.go
@@ -3,21 +3,211 @@ package main
 
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
+	"github.com/pion/rtcp"
+	"github.com/pion/webrtc"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"path"
+	"time"
 )
+
+const (
+	rtcpPLIInterval = time.Second * 3
+)
+
+type Invitation struct {
+	Guest string
+	Conference string
+}
+
+func newInvitation(conference string, guest string) *Invitation {
+	invitation := Invitation{
+		Conference: conference,
+		Guest:     guest,
+	}
+
+	return &invitation
+}
+
+type Conference struct {
+	Users *[]User
+	Initiator *User
+	Uuid *uuid.UUID
+}
+
+func (conference *Conference) addUser(user *User)  {
+	users := conference.Users
+	*conference.Users = append(*users, *user)
+}
+
+func newConference(initiator *User) *Conference {
+	users := make([]User, 0)
+	uuid, _ := uuid.NewUUID()
+	return &Conference{
+		Users: &users,
+		Initiator: initiator,
+		Uuid: &uuid,
+	}
+}
+
+type ConferenceList struct {
+	conferences *[]Conference
+}
+
+func (conferenceList *ConferenceList) newConference(initiator *User) *Conference {
+	conference := newConference(initiator)
+	conferences := conferenceList.conferences
+	*conferenceList.conferences = append(*conferences, *conference)
+	return conference
+}
+
+func newConferenceList() *ConferenceList {
+	conferences := make([]Conference, 0)
+	return &ConferenceList{conferences: &conferences}
+}
 
 type User struct {
 	Username string
+	Invitation chan *Invitation
+	RemoteConnection *webrtc.PeerConnection
+	RemoteConnectionChannel chan *string
+}
+
+func (user User) getUsername() string {
+	return user.Username
+}
+
+func (user User) createRemoteConnection(offer *WebsocketOffer) string {
+	fmt.Println(fmt.Sprintf("Creating remote connection for user %s", user.getUsername()))
+	recvOnlyOffer := webrtc.SessionDescription{}
+	DecodeOffer(offer.LocalDescription, &recvOnlyOffer)
+	mediaEngine := webrtc.MediaEngine{}
+	err := mediaEngine.PopulateFromSDP(recvOnlyOffer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create the API object with the MediaEngine
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(mediaEngine))
+
+	peerConnectionConfig := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+
+	peerConnection, err := api.NewPeerConnection(peerConnectionConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err = peerConnection.AddTransceiverFromKind(webrtc.RTPCodecTypeAudio); err != nil {
+		panic(err)
+	} else if _, err = peerConnection.AddTransceiverFromKind(webrtc.RTPCodecTypeVideo); err != nil {
+		panic(err)
+	}
+
+	localTrackChan := make(chan *webrtc.Track)
+	// Set a handler for when a new remote track starts, this just distributes all our packets
+	// to connected peers
+	peerConnection.OnTrack(func(remoteTrack *webrtc.Track, receiver *webrtc.RTPReceiver) {
+		// Send a PLI on an interval so that the publisher is pushing a keyframe every rtcpPLIInterval
+		// This can be less wasteful by processing incoming RTCP events, then we would emit a NACK/PLI when a viewer requests it
+		go func() {
+			ticker := time.NewTicker(rtcpPLIInterval)
+			for range ticker.C {
+				if rtcpSendErr := peerConnection.WriteRTCP([]rtcp.Packet{&rtcp.PictureLossIndication{MediaSSRC: remoteTrack.SSRC()}}); rtcpSendErr != nil {
+					fmt.Println(rtcpSendErr)
+				}
+			}
+		}()
+
+		// Create a local track, all our SFU clients will be fed via this track
+		localTrack, newTrackErr := peerConnection.NewTrack(remoteTrack.PayloadType(), remoteTrack.SSRC(), "video", "pion")
+		if newTrackErr != nil {
+			panic(newTrackErr)
+		}
+		localTrackChan <- localTrack
+
+		rtpBuf := make([]byte, 1400)
+		for {
+			i, readErr := remoteTrack.Read(rtpBuf)
+			if readErr != nil {
+				panic(readErr)
+			}
+
+			// ErrClosedPipe means we don't have any subscribers, this is ok if no peers have connected yet
+			if _, err = localTrack.Write(rtpBuf[:i]); err != nil && err != io.ErrClosedPipe {
+				panic(err)
+			}
+		}
+	})
+
+	// Set the remote SessionDescription
+	err = peerConnection.SetRemoteDescription(recvOnlyOffer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create answer
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	err = peerConnection.SetLocalDescription(answer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the LocalDescription and take it to base64 so we can paste in browser
+	user.RemoteConnection = peerConnection
+	encodedAnswer := EncodeOffer(answer)
+	select {
+		case user.RemoteConnectionChannel <- &encodedAnswer:
+		default:
+	}
+
+	return encodedAnswer
+}
+func newUser(Username string) *User {
+	invitationChannel := make(chan *Invitation, 1)
+	remoteConnectionChannel := make(chan *string, 1)
+	return &User{
+		Username: Username,
+		Invitation: invitationChannel,
+		RemoteConnectionChannel: remoteConnectionChannel,
+	}
 }
 
 type UserList struct {
 	users *[]User
+}
+
+func newUserList() *UserList {
+	users := make([]User, 0)
+	return &UserList{users: &users}
+}
+
+func (userList *UserList) has(user *User) bool {
+	found := false
+	for _, currentUser := range *userList.users {
+		if currentUser.getUsername() == user.getUsername() {
+			found = true
+			break
+		}
+	}
+	return found
 }
 
 func (userList *UserList) append(user *User)  {
@@ -25,7 +215,32 @@ func (userList *UserList) append(user *User)  {
 		newList := make([]User, 0)
 		userList.users = &newList
 	}
-	*userList.users = append(*userList.users, *user)
+	if ! userList.has(user)	{
+		*userList.users = append(*userList.users, *user)
+	}
+}
+
+func (userList *UserList) remove(user *User) {
+	if userList.users != nil {
+		dereferencedUserList := *userList.users
+		userListLength := len(dereferencedUserList)
+		if userListLength > 0 {
+			for key, item := range dereferencedUserList {
+				if item.Username == user.Username {
+					if key < len(dereferencedUserList) - 1 {
+						dereferencedUserList[key] = dereferencedUserList[len(dereferencedUserList) - 1]
+					}
+
+					if userListLength > 1 {
+						dereferencedUserList = dereferencedUserList[:len(dereferencedUserList)-2]
+					} else {
+						dereferencedUserList = make([]User, 0)
+					}
+				}
+			}
+		}
+		userList.users = &dereferencedUserList
+	}
 }
 
 func (userList UserList) isEmpty() bool {
@@ -36,16 +251,25 @@ func (userList UserList) isEmpty() bool {
 	}
 }
 
+func (userList UserList) getFirst() *User {
+	userSlice := *userList.users
+	if len(userSlice) == 0 {
+		return nil
+	}
+	user := userSlice[0]
+	return &user
+}
+
 type WebsocketMessage struct {
 	Object string
 	Action string
-	UserName string
+	Data string
 }
 
-func (userList UserList) diffTo(anotherUserList *UserList) (UserList, UserList) {
+func (userList UserList) diffTo(anotherUserList *UserList) (*UserList, *UserList) {
 	var found bool
-	var added UserList
-	var removed UserList
+	var added = newUserList()
+	var removed = newUserList()
 
 	if ! userList.isEmpty() {
 		for _, user := range *userList.users {
@@ -93,15 +317,25 @@ type RTCTemplate struct {
 type WebsocketHandler struct {
 	upgrader *websocket.Upgrader
 	userList *UserList
+	conferenceList *ConferenceList
 }
 
-func (handler WebsocketHandler) addUser(user *User) {
-	if handler.userList.users == nil {
-		userList := make([]User, 0)
-		handler.userList.users = &userList
+func newWebsocketHandler(upgrader *websocket.Upgrader, userList *UserList) *WebsocketHandler {
+	return &WebsocketHandler{
+		upgrader:       upgrader,
+		userList:       userList,
+		conferenceList: newConferenceList(),
 	}
-	*handler.userList.users = append(*handler.userList.users, *user)
 }
+func (appInstance *AppInstance) addUser(user *User) {
+	fmt.Println(fmt.Sprintf("AppInstance %p Adding User \"%s\" to handler", &appInstance, user.Username))
+	*appInstance.userList.users = append(*appInstance.userList.users, *user)
+}
+
+func (appInstance *AppInstance) getMyself() *User {
+	return appInstance.myself
+}
+
 func (template *RTCTemplate) handleWebRTCJS(w http.ResponseWriter, r *http.Request) {
 	templateName := path.Base(r.URL.Path)
 	log.Print("Serving ", templateName)
@@ -113,30 +347,124 @@ func (template *RTCTemplate) handleWebRTCJS(w http.ResponseWriter, r *http.Reque
 	}
 }
 
-func writeUserMessages(c *websocket.Conn, list UserList, action string) {
+func writeUserMessages(c *websocket.Conn, list *UserList, action string) {
 	if ! list.isEmpty() {
 		for _, item := range *list.users {
 			message, _ := json.Marshal(WebsocketMessage{
 				Object:   "User",
 				Action:   action,
-				UserName: item.Username,
+				Data: item.Username,
 			})
 			c.WriteMessage(websocket.TextMessage, message)
 		}
 	}
 }
 
-func (handler WebsocketHandler) handleUserAction(connection *websocket.Conn, websocketMessage *WebsocketMessage) {
+func writeInvitation(c *websocket.Conn, invitation *Invitation) {
+	data, _ := json.Marshal(invitation)
+	message, _ := json.Marshal(WebsocketMessage{
+		Object:   "Call",
+		Action:   "invite",
+		Data: string(data),
+	})
+	c.WriteMessage(websocket.TextMessage, message)
+}
+
+func writeAnswer(c *websocket.Conn, answer *string) {
+	fmt.Println("Writing answer to call")
+	message, _ := json.Marshal(WebsocketMessage{
+		Object:   "Call",
+		Action:   "answer",
+		Data: *answer,
+	})
+	c.WriteMessage(websocket.TextMessage, message)
+}
+
+func (appInstance *AppInstance) handleUserAction(websocketMessage *WebsocketMessage) {
 	switch websocketMessage.Action {
 		case "add":
-			handler.addUser(&User{Username: websocketMessage.UserName})
+			user := newUser(websocketMessage.Data)
+			appInstance.myself = user
+			appInstance.addUser(user)
+			break
 	}
 }
 
-func (handler WebsocketHandler) readMessages(c *websocket.Conn) {
+func (appInstance *AppInstance) isLoggedIn() bool {
+	return ! (appInstance.myself == nil)
+}
+
+type WebsocketOffer struct {
+	Users []string
+	LocalDescription string
+}
+
+func DecodeOffer(in string, obj interface{}) {
+	b, err := base64.StdEncoding.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(b, obj)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func EncodeOffer(obj interface{}) string {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func (appInstance *AppInstance) createConference(initiator *User) *Conference {
+	return appInstance.handler.conferenceList.newConference(initiator)
+}
+func (appInstance *AppInstance) handleCallAction(
+	connection *websocket.Conn,
+	websocketMessage *WebsocketMessage,
+	userList *UserList) {
+	switch websocketMessage.Action {
+		case "offer":
+			websocketOffer := &WebsocketOffer{}
+			myself := appInstance.getMyself()
+			ownUserName := myself.getUsername()
+
+			json.Unmarshal([]byte(websocketMessage.Data), websocketOffer)
+			myself.createRemoteConnection(websocketOffer)
+			conference := appInstance.createConference(myself)
+			conference.addUser(myself)
+
+			for _, offerUserName := range websocketOffer.Users {
+				for _, user := range *userList.users {
+					if user.Username == offerUserName {
+						conference.addUser(&user)
+						fmt.Println(fmt.Sprintf("Inviting %s", user.getUsername()))
+						select {
+							case user.Invitation <- newInvitation(conference.Uuid.String(), ownUserName):
+							default:
+								fmt.Println(fmt.Sprintf("Invitation Channel of %s is already full", user.getUsername()))
+						}
+					}
+				}
+			}
+
+			break
+		case "accepted":
+			myself := appInstance.getMyself()
+			websocketOffer := &WebsocketOffer{}
+			json.Unmarshal([]byte(websocketMessage.Data), websocketOffer)
+			myself.createRemoteConnection(websocketOffer)
+	}
+}
+
+func (appInstance *AppInstance) readMessages() {
 	for {
 		websocketMessage := WebsocketMessage{}
-		err := c.ReadJSON(&websocketMessage)
+		err := appInstance.websocketConnection.ReadJSON(&websocketMessage)
 		if err != nil {
 			log.Println("read:", err)
 			break
@@ -144,15 +472,74 @@ func (handler WebsocketHandler) readMessages(c *websocket.Conn) {
 
 		switch websocketMessage.Object {
 			case "User":
-				handler.handleUserAction(c, &websocketMessage)
+				appInstance.handleUserAction(&websocketMessage)
+				break
+			case "Call":
+				appInstance.handleCallAction(appInstance.websocketConnection, &websocketMessage, appInstance.userList)
 				break
 		}
 	}
 
 }
+
+type AppInstance struct {
+	userList *UserList
+	websocketConnection *websocket.Conn
+	myself *User
+	handler WebsocketHandler
+}
+
+func (appInstance *AppInstance) run(userList *UserList) {
+	var inAppUserlistOnly *UserList
+	var inGlobalUserlistOnly *UserList
+	isInitialized := false
+	go appInstance.readMessages()
+	for {
+		if ! appInstance.isLoggedIn() {
+			continue
+		}
+
+		if  ! isInitialized {
+			for _, currentUser := range *userList.users {
+				initUserList := newUserList()
+				if &currentUser != appInstance.myself {
+					appInstance.userList.append(&currentUser)
+					initUserList.append(&currentUser)
+				}
+				writeUserMessages(appInstance.websocketConnection, initUserList, "add")
+				initUserList = nil
+			}
+			isInitialized = true
+		}
+		inAppUserlistOnly, inGlobalUserlistOnly = appInstance.userList.diffTo(userList)
+
+		writeUserMessages(appInstance.websocketConnection, inAppUserlistOnly, "add")
+
+		writeUserMessages(appInstance.websocketConnection, inGlobalUserlistOnly, "add")
+
+		for _, user := range *inAppUserlistOnly.users {
+			userList.append(&user)
+		}
+
+		for _, user := range *inGlobalUserlistOnly.users {
+			appInstance.userList.append(&user)
+		}
+
+		if ! appInstance.userList.isEmpty() {
+			select {
+			case invitation := <-appInstance.getMyself().Invitation:
+				fmt.Println(fmt.Sprintf("Sending invitation to %s", appInstance.getMyself().getUsername()))
+				writeInvitation(appInstance.websocketConnection, invitation)
+			case answer := <-appInstance.getMyself().RemoteConnectionChannel:
+				writeAnswer(appInstance.websocketConnection, answer)
+			default:
+
+			}
+		}
+
+	}
+}
 func (handler WebsocketHandler) handleWebsocket(w http.ResponseWriter, r *http.Request) {
-	var added UserList
-	var removed UserList
 	c, err := handler.upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
@@ -160,17 +547,13 @@ func (handler WebsocketHandler) handleWebsocket(w http.ResponseWriter, r *http.R
 	}
 	defer c.Close()
 
-	userList := &UserList{}
-
-	go handler.readMessages(c)
-	for {
-
-		added, removed = handler.userList.diffTo(userList)
-
-		writeUserMessages(c, added, "add")
-		writeUserMessages(c, removed, "remove")
-
+	userList := newUserList()
+	appInstance := AppInstance{
+		userList: userList,
+		websocketConnection: c,
+		handler: handler,
 	}
+	appInstance.run(handler.userList)
 }
 
 func createTemplate(name string) *template.Template {
@@ -188,8 +571,8 @@ func main() {
 		port,
 		createTemplate("/templates/js/webRTC.js"),
 	}
-	userList := &UserList{}
-	websocketHandler := &WebsocketHandler{&upgrader, userList}
+	userList := newUserList()
+	websocketHandler := newWebsocketHandler(&upgrader, userList)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/websocket", websocketHandler.handleWebsocket)
 	http.HandleFunc("/templates/js/webRTC.js", rtcTemplate.handleWebRTCJS)
--- static/js/webRTC.js
+++ static/js/webRTC.js
@@ -1,33 +1,344 @@
-window.addEventListener("load", function(evt) {
-    var register_login = function()
+window.addEventListener("load", function() {
+    let cUserList = function()
     {
-        var login_button = document.getElementById('login_button'),
-            username_field = document.getElementById('username'),
-            ws, username;
+        let users = [], selection = null, myself = null;
 
-        login_button.addEventListener('click', function (ev) {
-            ws = new WebSocket("ws://"+window.location.hostname+':'+window.websocketPort+"/websocket");
-            ws.onopen = function(evt) {
-                console.log("OPEN");
-                document.body.className = 'user_list';
+        this.add = function (user) {
+            users.push(user)
+        };
 
-                ws.send(JSON.stringify({"Object": "User", "Action": "add", "UserName": username_field.value}));
+        this.remove = function (user) {
+            let elem;
+            for (let i = 0; i<this.getLength();)
+            {
+                elem = users[i]
+                if (elem.getUserName() === user.getUserName())
+                {
+                    users.splice(i, 1);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        };
+
+        this.getLength = function()
+        {
+            return users.length;
+        };
+
+        this.getUserAt = function(index)
+        {
+            return users[index];
+        };
+
+        this.getUserByUserName = function(userName)
+        {
+            let user = null, currentUser;
+            for (let i = 0; i<this.getLength();i++)
+            {
+                currentUser = this.getUserAt(i);
+                if (currentUser.getUserName() === userName)
+                {
+                    user = currentUser;
+                    break;
+                }
+            }
+            return user;
+        };
+
+        this.getSelection = function () {
+            if (selection === null)
+            {
+                selection = new cUserList();
             }
 
-            ws.onclose = function(evt) {
+            return selection;
+        };
+
+        this.isUserSelected = function(user) {
+            let selection = this.getSelection(), isSelected = false;
+            for (let i = 0; i<selection.getLength();i++)
+            {
+                if (selection.getUserAt(i).getUserName() === user.getUserName())
+                {
+                    isSelected = true;
+                    break;
+                }
+            }
+
+            return isSelected;
+        };
+
+        this.toggleUserSelected = function (user)
+        {
+            if (this.isUserSelected(user))
+            {
+                user.getDomElement().classList.remove('selected');
+                this.getSelection().remove(user);
+            }
+            else
+            {
+                user.getDomElement().classList.add('selected');
+                this.getSelection().add(user);
+            }
+        };
+
+        this.setMyself = function(paramMyself)
+        {
+            myself = paramMyself;
+        }
+
+        this.getSelectedUsers = function(withMyself)
+        {
+            if (typeof withMyself == 'undefined')
+            {
+                withMyself = true;
+            }
+
+            let users = [], selection = this.getSelection(), currentUser;
+            for (let i=0; i<selection.getLength();i++)
+            {
+                currentUser = selection.getUserAt(i);
+                if ((!withMyself && currentUser !== myself) || withMyself)
+                {
+                    users.push(currentUser);
+                }
+            }
+
+            return users
+        }
+
+        this.getSelectedUserNames = function(withMyself)
+        {
+            let users = this.getSelectedUsers(withMyself), userNames = []
+
+            for (let i=0; i<users.length;i++)
+            {
+                userNames.push(users[i].getUserName());
+            }
+
+            return userNames;
+        }
+    };
+
+    let cUser = function(userName)
+    {
+        let domElement;
+
+        domElement = document.createElement('LI');
+        domElement.innerText = userName;
+
+        this.getUserName = function()
+        {
+            return userName;
+        }
+
+        this.getDomElement = function () {
+            return domElement;
+        };
+    };
+
+    let cVideo = function()
+    {
+        let domElement = document.createElement('video');
+
+        this.getDomElement = function()
+        {
+            return domElement;
+        };
+
+        this.setSrcObject = function (paramSrcObject) {
+            this.getDomElement().srcObject = paramSrcObject;
+        };
+    }
+
+    let cVideoList = function ()
+    {
+        let videos = [];
+
+        this.createVideo = function()
+        {
+            let video = new cVideo();
+            videos.push(video);
+
+            return video;
+        };
+
+        this.getVideos = function () {
+            return videos;
+        };
+    };
+
+    let registerLogin = function () {
+        let loginButton = document.getElementById('login_button'),
+            usernameField = document.getElementById('username'),
+            userListDomElement = document.getElementById('user_list'),
+            videoListDomElement = document.getElementById('video_list'),
+            userList = new cUserList(),
+            ws,
+            myself,
+            videoList = new cVideoList(),
+            peerConnection = null,
+            app = this;
+
+        this.getPeerConnection = function()
+        {
+            if (peerConnection === null) {
+                peerConnection = new RTCPeerConnection({
+                    iceServers: [
+                        {
+                            urls: 'stun:stun.l.google.com:19302'
+                        }
+                    ]
+                });
+
+                navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+                    .then(stream => {
+                        stream.getTracks().forEach(function(track) {
+                            peerConnection.addTrack(track, stream);
+                        });
+                        let video = videoList.createVideo()
+                        video.setSrcObject(stream);
+                        videoListDomElement.appendChild(video.getDomElement())
+                        peerConnection.createOffer().then(d => {
+                            console.log('Offer created');
+                            peerConnection.setLocalDescription(d);
+                        }).catch(console.log);
+                    }).catch(console.log);
+            }
+
+            peerConnection.oniceconnectionstatechange = () => console.log('ICE Connection state changed ', peerConnection.iceConnectionState)
+
+            return peerConnection;
+        }
+
+        let dispatchUserEvent = function (eventData)
+        {
+            let user;
+            switch (eventData.Action)
+            {
+                case "add":
+                    let isMyself = false;
+                    if (eventData.Data === myself.getUserName())
+                    {
+                        user = myself;
+                        isMyself = true;
+                    }
+                    else
+                    {
+                        user = new cUser(eventData.Data);
+                    }
+
+                    let userDomElement = user.getDomElement();
+                    userListDomElement.appendChild(userDomElement);
+                    if (isMyself)
+                    {
+                        userList.toggleUserSelected(user);
+                        userDomElement.classList.add('myself')
+                    }
+                    else
+                    {
+                        userDomElement.addEventListener('click', function () {
+                            userList.toggleUserSelected(user);
+                        });
+                    }
+
+                    userList.add(user);
+                    break;
+                case "remove":
+                    user = userList.getUserByUserName(eventData.Data);
+                    if (userList.isUserSelected(user))
+                    {
+                        userList.toggleUserSelected(user);
+                    }
+                    userListDomElement.removeChild(user.getDomElement());
+                    user.getDomElement().remove();
+                    break;
+            }
+        };
+
+        let dispatchCallEvent = function(eventData)
+        {
+            let peerConnection;
+            switch (eventData.Action) {
+                case "invite":
+                        peerConnection = app.getPeerConnection();
+                        document.body.className = 'active_call';
+                        peerConnection.onicecandidate = event => {
+                            if (event.candidate === null) {
+                                let localDescription = btoa(JSON.stringify(peerConnection.localDescription));
+                                ws.send(JSON.stringify({
+                                    "Object": 'Call',
+                                    "Action": "accepted",
+                                    "Data": JSON.stringify({
+                                        "LocalDescription": localDescription
+                                    })
+                                }));
+                            }
+                        }
+                    break;
+                case "answer":
+                    peerConnection = app.getPeerConnection();
+                    peerConnection.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(eventData.Data)))).then(() => console.log('remote description set'))
+            }
+        }
+
+        loginButton.addEventListener('click', function () {
+            ws = new WebSocket("ws://" + window.location.hostname + ':' + window.websocketPort + "/websocket");
+            ws.onopen = function () {
+                console.log("OPEN");
+                document.body.className = 'user_list';
+                myself = new cUser(usernameField.value);
+                document.title = 'WebRTC Test :: ' + myself.getUserName();
+                userList.setMyself(myself);
+                ws.send(JSON.stringify({"Object": "User", "Action": "add", "Data": usernameField.value}));
+            }
+
+            ws.onclose = function () {
                 console.log("CLOSE");
                 ws = null;
             }
 
-            ws.onmessage = function(evt) {
+            ws.onmessage = function (evt) {
+                let eventData = JSON.parse(evt.data);
+                switch (eventData.Object) {
+                    case "User":
+                        dispatchUserEvent(eventData);
+                        break;
+                    case "Call":
+                        dispatchCallEvent(eventData);
+                }
                 console.log("RESPONSE: " + evt.data);
             }
 
-            ws.onerror = function(evt) {
+            ws.onerror = function (evt) {
                 console.log("ERROR: " + evt.data);
             }
-        });
-    }
 
-    register_login();
+            document.getElementById('call_button').addEventListener('click', function () {
+                let peerConnection = app.getPeerConnection(),
+                    localDescription;
+
+                document.body.className = 'active_call';
+
+                peerConnection.onicecandidate = event => {
+                    if (event.candidate === null) {
+                        localDescription = btoa(JSON.stringify(peerConnection.localDescription));
+                        ws.send(JSON.stringify({
+                            "Object": 'Call',
+                            "Action": "offer",
+                            "Data": JSON.stringify({
+                                    "Users": userList.getSelectedUserNames(false),
+                                    "LocalDescription": localDescription
+                                })
+                        }));
+                    }
+                }
+            });
+        });
+    };
+
+    registerLogin();
 });
